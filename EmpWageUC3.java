2c569ed276d99c547218a7ca982095c2c52ae70a
AakashJain903 <aakash.jain903@gmail.com>
2020-07-30 17:24:23 +0530

    Added : Calulate wage for full and part time employee

---
+++ EmpWageUC3.java
@@ -0,0 +1,21 @@
+class EmpWageUC3 {
+	public static void main(String args[]){
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
+		int empHrs = 0;
+		int empWage = 0;
+		int empRatePerHr = 20;
+
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_FULL_TIME)
+		{
+			empHrs = 8;
+		}if(empCheck == IS_PART_TIME){
+			empHrs = 4;
+		}else{
+			empHrs = 0;
+		}
+		empWage = empHrs * empRatePerHr;
+		System.out.println("Employee Wage : " + empWage);
+	}
+}
