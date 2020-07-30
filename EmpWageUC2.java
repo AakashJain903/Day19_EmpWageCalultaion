8bb91892e298ac66914b205280855e8dace7bc52
AakashJain903 <aakash.jain903@gmail.com>
2020-07-30 17:20:26 +0530

    Added : Calculate wage for full time employee

---
+++ EmpWageUC2.java
@@ -0,0 +1,17 @@
+class EmpWageUC2 {
+	public static void main(String args[]){
+		int IS_FULL_TIME = 1;
+		int empHrs = 0;
+		int empWage = 0;
+		int empRatePerHr = 20;
+
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else
+			empHrs = 0;
+
+		empWage = empHrs * empRatePerHr;
+		System.out.println("Employee Wage : " + empWage);
+	}
+}
